52df86870cae7a5a6be41bea251a5d1ad6e5f7d9
zxh <zxh_py@163.com>
2019-07-18 23:02:18 +0800

    add .

--- manager.py
+++ manager.py
@@ -4,12 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 class Config(object):
     # 配置SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1/Flask_Project'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1/Flaks_Project'
     SQLALCHEMY_TRACK_MODIFICATIONS = Flask
 
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 # 插件数据库
 db = SQLAlchemy(app)
 
