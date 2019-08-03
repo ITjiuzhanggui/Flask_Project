1ab4f7a06f35a5254cc05ed3081fba63d44632be
zxh <zxh_py@163.com>
2019-08-03 23:38:44 +0800

    add static

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,13 +1,16 @@
 # 2.1 导入蓝图对象
 from . import index_blue
+from flask import render_template, current_app
 
 
 # 2.2 使用蓝图对象实现路由
 @index_blue.route('/')
-def hello_world():
-    # logging.debug("this is debug")
-    # logging.info("this is debug")
-    # logging.warning("this is debug")
-    # logging.error("this is debug")
-    # logging.fatal("this is debug")
-    return 'hello world'
+def index():
+    return render_template('news/index.html')
+
+
+# 处理浏览器自动访问网站图标的路由
+@index_blue.route('/favicon.ico')
+def favicon_ico():
+    # 使用current_app,发送静态文件(图片/文本/js/html)
+    return current_app.send_static_file('news/favicon.ico')
