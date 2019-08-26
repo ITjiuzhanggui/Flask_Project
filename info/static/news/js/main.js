02aefbc243f83f57ce34ee23a40d1ba27ee78f72
zxh <zxh_py@163.com>
2019-08-27 00:16:41 +0800

    add register

--- info/static/news/js/main.js
+++ info/static/news/js/main.js
@@ -146,7 +146,27 @@ $(function () {
         }
 
         // 发起注册请求
+        var params = {
+            "mobile": mobile,
+            "smscode": smscode,
+            "password": password,
+        }
 
+        $.ajax({
+            url: "/passport/register",
+            type: "post",
+            data: JSON.stringify(params),
+            contentType: "application/json",
+            success: function (resp) {
+                if (resp.errno == "0") {
+                    // 刷新当前界面
+                    location.reload()
+                } else {
+                    $("#register-password-err").html(resp.errmsg)
+                    $("#register-password-err").show()
+                }
+            }
+        })
     })
 })
 
